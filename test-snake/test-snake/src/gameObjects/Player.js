febe63fae4349bf1c26c1ee9b942a0ca67242096
nyamic <kimh@ewha.ac.kr>
2025-08-04 03:09:57 +0900

    Feat: 튜토리얼게임 구현

---
+++ test-snake/test-snake/src/gameObjects/Player.js
@@ -0,0 +1,32 @@
+export class Player extends Phaser.Physics.Arcade.Sprite
+{
+    constructor(scene, x, y)
+    {
+
+    }
+
+    initAnimations()
+    {
+
+    }
+
+    moveLeft()
+    {
+
+    }
+
+    moveRight()
+    {
+
+    }
+
+    idle()
+    {
+
+    }
+
+    jump()
+    {
+        
+    }
+}
